76a0672953c884a675b19861f5cbc85862b97e53
kokweikhong <kokweikhong@gmail.com>
2022-03-12 14:32:47 +0800

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,21 @@ var swiper = new Swiper(".projects", {
   loop: true,
   speed: 500,
   spaceBetween: 30,
-  effect: "fade",
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
